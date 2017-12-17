c42f5d7df255e85f42bdfeedd7ba385e0ad2ad89
samirb <sam.bkh@hotmail.com>
2017-12-17 17:49:48 +0100

    Version 1.0 de l'expo initial commit

---
+++ presentationide.php
@@ -0,0 +1,21 @@
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script>
+ $(document).ready(function(){
+     setInterval(ajaxcall, 1000);
+ });
+ function ajaxcall(){
+     $.ajax({
+         url: 'gettime.php',
+         success: function(data) {
+             data = data.split(':');
+             $('#hours').html(data[0]);
+             $('#minutes').html(data[1]);
+             $('#seconds').html(data[2]);
+         }
+     });
+ }
+</script>
+<span> Nous sommes le <?php echo date ('d/m/Y') ?> ,  Il est </span>
+<span id="hours">0</span>:<span id="minutes">0</span>:<span id="seconds">0</span>
+
+
